75f7791deea0416f5358ccde2b27dfcd4b2368c7
AnantSingh08 <anant.comics2000@gmail.com>
2022-05-18 20:26:34 +0530

    After code review

--- my-app/src/components/Signin/Signin.js
+++ my-app/src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://boiling-hamlet-21239.herokuapp.com//signin', {
+    fetch('https://boiling-hamlet-21239.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
